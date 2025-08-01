9bb230509dca5a890a7e66e4c1fd2ad50543d2a8
Egor Ulybin <egorulybin@gmail.com>
2025-08-01 23:40:21 +0200

    changed the btn color to secondary

--- react-hello-main/src/js/components/Home.jsx
+++ react-hello-main/src/js/components/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => {
   const decrementCounter = () => setCounter(counter - 1);
   return (
     <div className='text-center d-flex justify-content-center align-items-center flex-column mt-5 gap-3'>
-      <button className='btn btn-success btn-lg' onClick={incrementCounter}>
+      <button className='btn btn-secondary btn-lg' onClick={incrementCounter}>
         Increment Counter
       </button>
       <span className='display-1'>Counter: {counter}</span>
-      <button className='btn btn-success btn-lg' onClick={decrementCounter}>
+      <button className='btn btn-secondary btn-lg' onClick={decrementCounter}>
         Decrement Counter
       </button>
     </div>
