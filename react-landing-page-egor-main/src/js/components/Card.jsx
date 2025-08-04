374a84ea5c668a251c8348c71fe66f545116cfa4
Egor Ulybin <egorulybin@gmail.com>
2025-08-04 20:38:44 +0200

    added props to the card component

--- react-landing-page-egor-main/src/js/components/Card.jsx
+++ react-landing-page-egor-main/src/js/components/Card.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import greyBg from '../../img/grey_bg.png';
 
-const Card = () => {
+const Card = ({ title, description }) => {
   return (
     <div className='card'>
       <div className='card-body'>
-        <img src={greyBg} className='card-img-top img-fluid' alt='grey background' />
-        <h5 className='card-title'>Card title</h5>
-        <p className='card-text'>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
+        <img src={greyBg} className='card-img-top' alt='grey background' />
+        <h5 className='card-title mt-2'>{title}</h5>
+        <p className='card-text'>{description}</p>
         <a href='#' className='btn btn-primary'>
           Go somewhere
         </a>
--- react-landing-page-egor-main/src/js/components/Container.jsx
+++ react-landing-page-egor-main/src/js/components/Container.jsx
@@ -13,16 +13,28 @@ const Container = () => {
 
       <div className='row'>
         <div className='col-md-6 col-lg-3'>
-          <Card />
+          <Card
+            title='Title 1'
+            description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis excepturi.'
+          />
         </div>
         <div className='col-md-6 col-lg-3'>
-          <Card />
+          <Card
+            title='Title 2'
+            description='Some other and brief description for the second card! Just saying whatever...'
+          />
         </div>
         <div className='col-md-6 col-lg-3'>
-          <Card />
+          <Card
+            title='Title 3'
+            description='dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+          />
         </div>
         <div className='col-md-6 col-lg-3'>
-          <Card />
+          <Card
+            title='Title 4'
+            description='There are many variations of passages of Lorem Ipsum available, but the majority have.'
+          />
         </div>
       </div>
     </div>
