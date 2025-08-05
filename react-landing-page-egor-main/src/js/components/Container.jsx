b8004f11b6792bf9962470504872746620939790
Egor Ulybin <egorulybin@gmail.com>
2025-08-05 12:59:47 +0200

    included the map method to dynamically add data to Card component

--- react-landing-page-egor-main/src/js/components/Container.jsx
+++ react-landing-page-egor-main/src/js/components/Container.jsx
@@ -2,7 +2,35 @@ import React from 'react';
 import Card from './Card';
 import Jumbotron from './Jumbotron';
 
+const cardData = [
+  {
+    title: 'Title 1',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis excepturi.',
+  },
+  {
+    title: 'Title 2',
+    description: 'Some other and brief description for the second card! Just saying whatever...',
+  },
+  {
+    title: 'Title 3',
+    description:
+      'dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  },
+  {
+    title: 'Title 4',
+    description: 'There are many variations of passages of Lorem Ipsum available, but the majority have.',
+  },
+];
+
 const Container = () => {
+  const insertCardData = cardData.map((card, index) => {
+    return (
+      <div key={index} className='col-md-6 col-lg-3'>
+        <Card title={card.title} description={card.description} />
+      </div>
+    );
+  });
+
   return (
     <div className='container'>
       <div className='row'>
@@ -11,32 +39,7 @@ const Container = () => {
         </div>
       </div>
 
-      <div className='row'>
-        <div className='col-md-6 col-lg-3'>
-          <Card
-            title='Title 1'
-            description='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis excepturi.'
-          />
-        </div>
-        <div className='col-md-6 col-lg-3'>
-          <Card
-            title='Title 2'
-            description='Some other and brief description for the second card! Just saying whatever...'
-          />
-        </div>
-        <div className='col-md-6 col-lg-3'>
-          <Card
-            title='Title 3'
-            description='dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
-          />
-        </div>
-        <div className='col-md-6 col-lg-3'>
-          <Card
-            title='Title 4'
-            description='There are many variations of passages of Lorem Ipsum available, but the majority have.'
-          />
-        </div>
-      </div>
+      <div className='row'>{insertCardData}</div>
     </div>
   );
 };
