429a5dd316c4fe9c3229cf57a8777dda85aa04cd
Egor Ulybin <egorulybin@gmail.com>
2025-08-06 19:13:57 +0200

    passed more props to Card and changed index to id

--- react-landing-page-egor-main/src/js/components/Card.jsx
+++ react-landing-page-egor-main/src/js/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import greyBg from '../../img/grey_bg.png';
 
-const Card = ({ title, description }) => {
+const Card = ({ title, description, buttonLabel, buttonLink }) => {
   return (
     <div className='card'>
       <div className='card-body'>
         <img src={greyBg} className='card-img-top' alt='grey background' />
         <h5 className='card-title mt-2'>{title}</h5>
         <p className='card-text'>{description}</p>
-        <a href='#' className='btn btn-primary'>
-          Go somewhere
+        <a href={buttonLink} target='_blank' className='btn btn-primary'>
+          {buttonLabel}
         </a>
       </div>
     </div>
--- react-landing-page-egor-main/src/js/components/Container.jsx
+++ react-landing-page-egor-main/src/js/components/Container.jsx
@@ -6,27 +6,43 @@ const cardData = [
   {
     title: 'Title 1',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis excepturi.',
+    id: 1,
+    buttonLabel: 'Go somewhere',
+    buttonLink: 'https://youtube.com',
   },
   {
     title: 'Title 2',
     description: 'Some other and brief description for the second card! Just saying whatever...',
+    id: 2,
+    buttonLabel: 'Go someplace',
+    buttonLink: 'https://github.com',
   },
   {
     title: 'Title 3',
-    description:
-      'dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    description: 'dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet, adipisicing.',
+    id: 3,
+    buttonLabel: 'Go anywhere',
+    buttonLink: 'https://google.com',
   },
   {
     title: 'Title 4',
-    description: 'There are many variations of passages of Lorem Ipsum available, but the majority have.',
+    description: 'There are many variations of passages of Lorem Ipsum available, but the majority.',
+    id: 4,
+    buttonLabel: 'Go everywhere',
+    buttonLink: 'https://facebook.com',
   },
 ];
 
 const Container = () => {
-  const insertCardData = cardData.map((card, index) => {
+  const insertCardData = cardData.map((card) => {
     return (
-      <div key={index} className='col-md-6 col-lg-3'>
-        <Card title={card.title} description={card.description} />
+      <div key={card.id} className='col-md-6 col-lg-3'>
+        <Card
+          title={card.title}
+          description={card.description}
+          buttonLabel={card.buttonLabel}
+          buttonLink={card.buttonLink}
+        />
       </div>
     );
   });
