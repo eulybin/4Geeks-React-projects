9ff4a1c0409391dc411793731f2c76a161bad16e
Egor Ulybin <egorulybin@gmail.com>
2025-08-11 20:50:21 +0200

    created a separate todoitem component and some logic

--- todo-list-react-egor-main/src/js/components/Home.jsx
+++ todo-list-react-egor-main/src/js/components/Home.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoItem from './TodoItem';
 
 import todosData from '../../data/todosData';
 
 const Home = () => {
+  const [todoInput, setTodoInput] = useState('');
+  const [data, setData] = useState(todosData);
+
+  const handleAddTodo = () => {
+    if (!todoInput === '') {
+      setData((prevTodosData) => [...prevTodosData, { todo: todoInput }]);
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    setData((prevTodoData) => {
+      return prevTodoData.filter((todo) => todo.id !== id);
+    });
+  };
   return (
-    <div className='text-center container bg-dark text-white'>
-      <h1>TODOS</h1>
+    <div className='main-container text-center'>
+      <div className='row'>
+        <div className='col'>
+          <h1 className='display-1'>todos</h1>
+          <div className='input-group mb-3'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Write your next todo...'
+              aria-label='Write your next todo...'
+              aria-describedby='button-addon2'
+              value={todoInput}
+              onChange={(e) => setTodoInput(e.target.value)}
+            />
+            <button onClick={handleAddTodo} className='btn btn-outline-primary' type='button' id='button-addon2'>
+              Add Todo
+            </button>
+          </div>
+        </div>
+      </div>
+
       <ul>
-        {todosData.map(({ todo, id }) => {
-          return <div key={id}>{todo}</div>;
+        {data.map(({ todo, id }) => {
+          return <TodoItem key={id} todo={todo} handleDeleteTodo={() => handleDeleteTodo(id)} />;
         })}
       </ul>
     </div>
---
+++ todo-list-react-egor-main/src/js/components/TodoItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TodoItem = ({ todo, handleDeleteTodo }) => {
+  return (
+    <div className='row'>
+      <div className='col'>
+        <div className='list-item p-3 my-3'>
+          <p>{todo}</p>
+          <button className='delete-btn btn btn-danger' onClick={handleDeleteTodo}>
+            <i className='fa-solid fa-trash' style={{ color: '#ffffff' }}></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
