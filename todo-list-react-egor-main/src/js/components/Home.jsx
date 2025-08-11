e1e35df3f33a3e202a766a9438aa05a94afaa63c
Egor Ulybin <egorulybin@gmail.com>
2025-08-11 21:12:11 +0200

    added the keydown function

--- todo-list-react-egor-main/src/js/components/Home.jsx
+++ todo-list-react-egor-main/src/js/components/Home.jsx
@@ -7,17 +7,24 @@ const Home = () => {
   const [todoInput, setTodoInput] = useState('');
   const [data, setData] = useState(todosData);
 
+  // -------- ADD TODO ON CLICK --------
   const handleAddTodo = () => {
     if (todoInput !== '') {
       setData((prevTodosData) => [...prevTodosData, { todo: todoInput, id: data.length + 1 }]);
       setTodoInput('');
     }
   };
-
+  // -------- DELETE TODO ON CLICK --------
   const handleDeleteTodo = (id) => {
     setData((prevTodoData) => {
       return prevTodoData.filter((todo) => todo.id !== id);
     });
+  };
+  // -------- ADD TODO ON ENTER --------
+  const handleAddOnEnter = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTodo();
+    }
   };
   return (
     <div className='main-container text-center'>
@@ -33,6 +40,7 @@ const Home = () => {
               aria-describedby='button-addon2'
               value={todoInput}
               onChange={(e) => setTodoInput(e.target.value)}
+              onKeyDown={handleAddOnEnter}
             />
             <button onClick={handleAddTodo} className='btn btn-outline-primary' type='button' id='button-addon2'>
               Add Todo
