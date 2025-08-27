b425796154fbd0b2c64ca514f96c2dc77a491020
Egor Ulybin <egorulybin@gmail.com>
2025-08-27 18:54:33 +0200

    created a user js file to get all todos for a specific user

---
+++ todo-list-react-egor-main/src/js/services/user.js
@@ -0,0 +1,15 @@
+import { BASE_URL } from '../utils/constants';
+
+// -------- GET ALL TODOS --------
+export const getAllUserTodos = async (userName) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/${userName}`);
+    if (!response.ok) {
+      throw new Error(`Could not fetch the todos for ${userName}!`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.error('Something went wrong: ' + err);
+  }
+};
