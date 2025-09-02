f4c68b56c30976a92bc969fb92b4932fc419d307
Egor Ulybin <egorulybin@gmail.com>
2025-09-02 19:45:20 +0200

    wrote the create new user POST request

--- todo-list-react-egor-main/src/js/services/user.js
+++ todo-list-react-egor-main/src/js/services/user.js
@@ -1,5 +1,31 @@
 import { BASE_URL } from '../utils/constants';
 
+// -------- CREATE NEW USER --------
+export const createNewUser = async (userName) => {
+  const newUserObj = {
+    name: userName,
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    body: JSON.stringify(newUserObj),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const response = await fetch(`${BASE_URL}/users/${userName}`, requestOptions);
+    if (!response.ok) {
+      throw new Error('Could not create a new user: ' + userName);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Something went wrong... ' + error);
+  }
+};
+
 // -------- GET ALL TODOS --------
 export const getAllUserTodos = async (userName) => {
   try {
